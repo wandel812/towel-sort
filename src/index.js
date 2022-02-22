8cec32a4d07f86c7e838c3783ef82a6ca862b57f
= <=>
2022-02-22 14:46:39 +0300

    feat: solve

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) {
+    return [];
+  }
+
+  let result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 === 0) {
+      result = result.concat(matrix[i]);
+    } else {
+      result = result.concat(matrix[i].slice().reverse());
+    }
+  }
+  console.log(result)
+  return result;
 }
